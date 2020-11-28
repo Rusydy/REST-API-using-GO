1618696ec5166938b7a1642d75c4c9d39a7a25e1
Rusydy <rusydymuhiddin@gmail.com>
2020-11-29 06:20:32 +0700

    Setting up Contact and contacts

--- main.go
+++ main.go
@@ -1,34 +1,44 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// Contact struct (Model)
+type Contact struct {
+	id    int    `json:"id"`
+	Name  string `json:"name"`
+	Phone string `json:"phone"`
+	Email string `json:"email"`
+}
+
+// Init contacts var as slice Contact struct
+var contacts []Contact
+
 // Main Function
 func main() {
-	
+
 	// Init router
 	r := mux.NewRouter()
 
 	// Hardcoded data - @todo: add database
 	contacts = append(
 		contacts, contact{
-			id: 1, Name: "Chandler Bing", Phone: "305-917-1301", Email: "chandlerbing@office.com"
-		}
+			id: 1, Name: "Chandler Bing", Phone: "305-917-1301", Email: "chandlerbing@office.com",
+		},
 	)
 	contacts = append(
 		contacts, contact{
-			id: 2, Name: "Ross Geller", Phone: "210-684-8953", Email: "rossgeller@office.com"
-		}
+			id: 2, Name: "Ross Geller", Phone: "210-684-8953", Email: "rossgeller@office.com",
+		},
 	)
 	contacts = append(
 		contacts, contact{
-			id: 3, Name: "Rachel Green", Phone: "765-338-0312", Email: "rachelgreen@office.com"
-		}
+			id: 3, Name: "Rachel Green", Phone: "765-338-0312", Email: "rachelgreen@office.com",
+		},
 	)
 
 	// Route handles & Endpoints
